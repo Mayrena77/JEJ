78ad8e077e8d10942ac738293212648959f205ba
Jessica Romero <romerojmc77@gmail.com>
2017-07-24 16:37:09 -0400

    indent

--- jej-music/main.py
+++ jej-music/main.py
@@ -3,8 +3,8 @@ import webapp2
 import os
 from google.appengine.ext import ndb
 
-    jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+jinja_environment = jinja2.Environment(
+loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
