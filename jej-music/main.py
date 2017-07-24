e9082b4e99c2ee116cb49f31564d6da7663be590
Emma Landry <emmkatlan123@gmail.com>
2017-07-24 12:53:08 -0400

    just started jinja stuff

--- jej-music/main.py
+++ jej-music/main.py
@@ -3,10 +3,12 @@ import webapp2
 import os
 from google.appengine.ext import ndb
 
+    jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
