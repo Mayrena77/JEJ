632291ff34b5cf32d2f442cb0384dbece6b5441e
Emma Landry <emmkatlan123@gmail.com>
2017-07-25 11:14:59 -0400

    added handler and fixed login stuffs

--- jej-music/main.py
+++ jej-music/main.py
@@ -8,58 +8,48 @@ jinja_environment = jinja2.Environment(
 loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class CssiUser(ndb.Model):
-
-  first_name = ndb.StringProperty()
-  last_name = ndb.StringProperty()
+    pass
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
+    template = jinja_environment.get_template('templates/homepage.html')
     user = users.get_current_user()
-    # If the user is logged in...
+
     if user:
       email_address = user.nickname()
       cssi_user = CssiUser.get_by_id(user.user_id())
       signout_link_html = '<a href="%s">sign out</a>' % (
           users.create_logout_url('/'))
-      # If the user has previously been to our site, we greet them!
-      if cssi_user:
-        self.response.write('''
-            Welcome %s %s (%s)! <br> %s <br>''' % (
-              cssi_user.first_name,
-              cssi_user.last_name,
+      self.response.write('''
+            Welcome %s! <br> %s <br>''' % (
               email_address,
               signout_link_html))
-      # If the user hasn't been to our site, we ask them to sign up
-      else:
-        self.response.write('''
-            Welcome to our site, %s!  Please sign up! <br>
-            <form method="post" action="/">
-            <input type="text" name="first_name">
-            <input type="text" name="last_name">
-            <input type="submit">
-            </form><br> %s <br>
-            ''' % (email_address, signout_link_html))
-    # Otherwise, the user isn't logged in!
+      self.response.out.write(template.render())
+
+
     else:
       self.response.write('''
         Please log in to use our site! <br>
         <a href="%s"> <img src = "JEJ_logo_words.jpg"/> </a>''' % (
-          users.create_login_url('/')))
+          users.create_login_url('/home')))
+
 
   def post(self):
+    template = jinja_environment.get_template('templates/homepage.html')
     user = users.get_current_user()
     if not user:
-      # You shouldn't be able to get here without being logged in
       self.error(500)
       return
     cssi_user = CssiUser(
-        first_name=self.request.get('first_name'),
-        last_name=self.request.get('last_name'),
         id=user.user_id())
     cssi_user.put()
-    self.response.write('Thanks for signing up, %s!' %
-        cssi_user.first_name)
+    self.response.out.write(template.render())
+    self.response.write('Thanks for signing up!')
 
+class HomepageHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/homepage.html')
+        self.response.out.write(template.render())
 
 class InputHandler(webapp2.RequestHandler):
     def get(self):
@@ -73,6 +63,7 @@ class ResultsHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/home', HomepageHandler),
     ('/input', InputHandler),
     ('/results', ResultsHandler)
 ], debug=True)
