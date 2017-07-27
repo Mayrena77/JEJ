415da7bd0a160989c69b274a16c531efecb81a36
John Sosa <jsosa1772@gmail.com>
2017-07-27 18:21:05 -0400

    Just in case

--- jej-music/main.py
+++ jej-music/main.py
@@ -14,15 +14,7 @@ class Song(ndb.Model):
     genre = ndb.StringProperty()
     artist = ndb.StringProperty()
     song_name = ndb.StringProperty()
-<<<<<<< HEAD
     user_key = ndb.KeyProperty(CssiUser)
-=======
-    CssiUser_key = ndb.KeyProperty(CssiUser)
-
-class CssiUser(ndb.Model):
-    first_name = ndb.StringProperty()
-    pass
->>>>>>> 60c1e4c726b2a16b4bb0b10137e14dcfa2e5e5d1
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
@@ -68,14 +60,10 @@ class MainHandler(webapp2.RequestHandler):
     self.response.out.write(template.render())
     self.response.write('Thanks for signing up!')
 
-
-
 class NameHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/name.html')
         self.response.write(template.render())
-
-
     def post(self):
         user = users.get_current_user()
         user_Id = CssiUser(
@@ -84,7 +72,6 @@ class NameHandler(webapp2.RequestHandler):
         )
         user_Id.put()
         self.redirect('/')
-
 
 
 
@@ -106,7 +93,7 @@ class ResultsHandler(webapp2.RequestHandler):
         genre = self.request.get('genre'),
         song_name = self.request.get('song_name'),
         artist = self.request.get('artist'),
-        user_key = cssi_user.key
+        user_key = cssi_user.key2
         )
         user_Input.put()
 
