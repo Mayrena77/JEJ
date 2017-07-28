652d342e56722527b220a39b639f59ad4757e882
John Sosa <jsosa1772@gmail.com>
2017-07-28 10:01:38 -0400

    Few changes before finally presenting (nothing big)

--- jej-music/main.py
+++ jej-music/main.py
@@ -88,7 +88,6 @@ class ResultsHandler(webapp2.RequestHandler):
         cssi_user = CssiUser.get_by_id(user.user_id())
         template = jinja_environment.get_template('templates/results.html')
 
-<<<<<<< HEAD
         """song_info = Song.query().fetch()
         song_genre = song_info.get
         genre_list = []
@@ -101,17 +100,6 @@ class ResultsHandler(webapp2.RequestHandler):
               if user.genre == cssi_user.genre:
                    print user
 """
-=======
-        # songs = Song.query().fetch()
-        #
-        #
-        # for s in songs
-        #       if user == cssi_user
-        #            continue
-        #       if user.genre == cssi_user.genre
-        #            print user
-
->>>>>>> 2b22d7256e23b38ffd97d3b09c0167fccd9d34e7
 
         self.response.write(template.render( { 'genre': self.request.get('genre'),
         'Name of Artist': self.request.get('artist'),
